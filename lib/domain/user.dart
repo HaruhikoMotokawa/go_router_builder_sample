eb89fcd2cf52eafc8659ff01bfd687896bfe24f7
本川　晴彦 <harx6009@icloud.com>
2025-06-08 15:28:11 +0900

    feat: Userモデルを追加し、HomeScreenでユーザーリストを表示する機能を実装

---
+++ lib/domain/user.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class User {
+  User({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.color,
+    required this.age,
+    required this.hobby,
+    required this.job,
+    required this.favoriteFood,
+  });
+  final String id;
+  final String name;
+  final IconData image;
+  final MaterialColor color;
+  final int age;
+  final String hobby;
+  final String job;
+  final String favoriteFood;
+
+  static List<User> list = [
+    User(
+      id: '1',
+      name: 'アリス',
+      image: Icons.face,
+      color: Colors.red,
+      age: 25,
+      hobby: '読書',
+      job: 'エンジニア',
+      favoriteFood: '寿司',
+    ),
+    User(
+      id: '2',
+      name: 'ボブ',
+      image: Icons.person,
+      color: Colors.blue,
+      age: 30,
+      hobby: 'サッカー',
+      job: 'デザイナー',
+      favoriteFood: 'ラーメン',
+    ),
+    User(
+      id: '3',
+      name: 'キャロル',
+      image: Icons.tag_faces,
+      color: Colors.green,
+      age: 28,
+      hobby: 'ピアノ',
+      job: '教師',
+      favoriteFood: 'カレー',
+    ),
+    User(
+      id: '4',
+      name: 'ダン',
+      image: Icons.sentiment_satisfied,
+      color: Colors.orange,
+      age: 35,
+      hobby: '釣り',
+      job: '医者',
+      favoriteFood: '焼肉',
+    ),
+    User(
+      id: '5',
+      name: 'エミリー',
+      image: Icons.emoji_people,
+      color: Colors.purple,
+      age: 22,
+      hobby: '映画鑑賞',
+      job: '学生',
+      favoriteFood: 'パスタ',
+    ),
+    User(
+      id: '6',
+      name: 'フランク',
+      image: Icons.mood,
+      color: Colors.cyan,
+      age: 40,
+      hobby: 'ジョギング',
+      job: 'シェフ',
+      favoriteFood: 'うどん',
+    ),
+    User(
+      id: '7',
+      name: 'グレース',
+      image: Icons.account_circle,
+      color: Colors.teal,
+      age: 27,
+      hobby: '写真',
+      job: 'カメラマン',
+      favoriteFood: 'オムライス',
+    ),
+    User(
+      id: '8',
+      name: 'ヘンリー',
+      image: Icons.person_outline,
+      color: Colors.indigo,
+      age: 33,
+      hobby: '登山',
+      job: '営業',
+      favoriteFood: 'ピザ',
+    ),
+    User(
+      id: '9',
+      name: 'イザベル',
+      image: Icons.emoji_emotions,
+      color: Colors.pink,
+      age: 29,
+      hobby: '料理',
+      job: 'ライター',
+      favoriteFood: 'サラダ',
+    ),
+    User(
+      id: '10',
+      name: 'ジャック',
+      image: Icons.sentiment_very_satisfied,
+      color: Colors.amber,
+      age: 38,
+      hobby: '旅行',
+      job: 'パイロット',
+      favoriteFood: '天ぷら',
+    ),
+  ];
+}
--- lib/presentation/screens/home/screen.dart
+++ lib/presentation/screens/home/screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router_builder_sample/core/router/route/route.dart';
+import 'package:go_router_builder_sample/domain/user.dart';
+import 'package:go_router_builder_sample/presentation/shared/help_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
@@ -7,9 +10,30 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+        centerTitle: false,
+        actions: const [
+          HelpButton(),
+        ],
+      ),
       body: Center(
-        child: Text('Hello World!'),
+        child: ListView.separated(
+          itemCount: User.list.length,
+          separatorBuilder: (context, index) => const Divider(),
+          itemBuilder: (context, index) {
+            final user = User.list[index];
+            return ListTile(
+              leading: Icon(user.image, color: user.color),
+              title: Text(user.name),
+              onTap: () => const DetailRoute().go(context),
+            );
+          },
+        ),
       ),
     );
   }
